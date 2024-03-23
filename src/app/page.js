1f081fe348f562fe7a51b930d2755104fe696ec7
Diya Sundeep <diyasundeep64@gmail.com>
2024-03-23 10:22:52 +0530

    toggle switch added

--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,12 @@ export default function Home() {
       <div className="absolute inset-0 ">
         <div className="flex items-center justify-center text-center text-white flex-col w-full">
           <div className="pt-10 text-9xl  font-custom">Flames</div>
+          <div className="flex justify-end">
+            <label class="inline-flex items-center cursor-pointer">
+              <input type="checkbox" value="" class="sr-only peer" />
+              <div class="relative w-11 h-6 bg-gray-600 peer-focus:outline-none    rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full  after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white  after:border after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-blue-400"></div>
+            </label>
+          </div>
           <div className="flex items-center justify-center font-aesth text-lg pt-10 font-semibold w-1/3 flex-col space-y-5 bg-green-100 bg-opacity-50 rounded-lg p-5">
             <div className="flex flex-row w-full justify-center ">
               <p className="text-2xl ">Enter name 1:</p>
