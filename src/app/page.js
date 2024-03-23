c1b9fcd72adbe04193bc0072e25c24ac9d5836c5
Diya Sundeep <diyasundeep64@gmail.com>
2024-03-23 09:18:24 +0530

    setting images

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
 import Image from "next/image";
-
+import green_city from "../../public/images/green.jpg";
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div className="relative h-screen">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <Image
+          src={green_city}
+          alt="green_city"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+      </div>
+      <div className="absolute inset-0 ">
+        <div className="flex items-center justify-center text-center text-white">
+          <div className="pt-10 text-4xl font-bold">Flames!</div>
+        </div>
+      </div>
+    </div>
+  );
 }
