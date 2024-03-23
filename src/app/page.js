22582cd79b6573d63b06b6424fbc28db8e144da1
Diya Sundeep <diyasundeep64@gmail.com>
2024-03-23 11:10:57 +0530

    fixed img change in theme

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@
 import { useState } from "react";
 import Image from "next/image";
 import green from "../../public/images/green.jpg";
+import night from "../../public/images/night.jpg";
+import paris from "../../public/images/paris.jpg";
+
 export default function Home() {
   const [mode, setMode] = useState(1);
 
@@ -13,28 +16,39 @@ export default function Home() {
     });
   };
 
+  // Define the image based on the mode
+  let bgImage;
+  if (mode === 1) {
+    bgImage = green;
+  } else if (mode === 2) {
+    bgImage = night;
+  } else {
+    bgImage = paris;
+  }
+
   return (
-    <div className="relative h-screen">
-      <div className="absolute inset-0 flex items-center justify-center">
+    <div className="flex flex-col h-screen">
+      <div className="flex justify-end">
+        <div
+          className={`custom-toggle ${
+            mode === 1 ? "mode1" : mode === 2 ? "mode2" : "mode3"
+          }`}
+          onClick={toggleMode}
+        >
+          <button className="p-1 bg-white text-black rounded">theme</button>
+        </div>
+      </div>
+
+      <div className="flex flex-1 items-center justify-center relative">
+        {/* Background image */}
         <Image
-          src={green}
-          alt="green"
+          src={bgImage}
+          alt="bgImage"
           className="absolute inset-0 w-full h-full object-cover"
         />
-      </div>
-      <div className="absolute inset-0">
-        <div className="flex items-center justify-center text-center text-white flex-col w-full">
+        <div className="absolute inset-0 flex items-center justify-center text-white flex-col w-full">
           <div className="pt-10 text-9xl font-custom">Flames</div>
-          <div className="flex justify-end">
-            <div
-              className={`custom-toggle ${
-                mode === 1 ? "mode1" : mode === 2 ? "mode2" : "mode3"
-              }`}
-              onClick={toggleMode}
-            >
-              <button className="p-1 bg-white text-black rounded">theme</button>
-            </div>
-          </div>
+
           <div className="flex items-center justify-center font-aesth text-lg pt-10 font-semibold w-1/3 flex-col space-y-5 bg-green-100 bg-opacity-50 rounded-lg p-5">
             {mode === 1 && (
               <>
