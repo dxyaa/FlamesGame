c078fdcea5d6f7e81a06a5b93be1a0a67b3942d1
Diya Sundeep <diyasundeep64@gmail.com>
2024-03-27 01:29:07 +0530

    working trying

--- src/app/components/circular.js
+++ src/app/components/circular.js
@@ -5,13 +5,14 @@ class CircularIterator {
   }
 
   getCharacterAt(index, str) {
-    const length = str.length; // Using the passed word instead of this.str
+    const length = str.length;
     console.log("length in circular : ", length);
     console.log("string: ", str);
     console.log("index in circular : ", index);
     const newIndex = (this.lastIndex + index - 1) % 6;
+    console.log("newind : ", newIndex);
     this.lastIndex = newIndex;
-    return str[newIndex]; // Returning character from the passed word
+    return str[newIndex];
   }
 }
 
